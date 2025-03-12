6ee3298c0c80f5d8d3f23abf562e69103b7f25eb
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-03-12 12:11:59 +0100

    Quitar status
    Mejor no complicarse de momento

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  status: { type: Number, default: 0 },
   role: { type: String, default: "user" },
   verificationCode: { type: String },
   attempts: { type: Number, default: 0 },
