0f3736abacae8347de0fca39107369423aa5e5f7
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-03-12 12:12:20 +0100

    Quitar campo status de controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ exports.register = async (req, res) => {
     const hashedPassword = await encrypt(password);
     // Generar código de verificación
     const verificationCode = generateVerificationCode();
-    // Crear el usuario
+    // Crear el usuario sin el campo status
     const user = new User({
       email,
       password: hashedPassword,
@@ -29,13 +29,12 @@ exports.register = async (req, res) => {
     await user.save();
     // Generar token JWT utilizando tokenSign
     const token = await tokenSign(user);
-    // Responder con los datos del usuario y el token
+    // Responder con los datos del usuario y el token (sin status)
     res.json({
       token,
       user: {
         _id: user._id,
         email: user.email,
-        status: user.status,
         role: user.role,
       },
     });
