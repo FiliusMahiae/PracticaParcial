c2c16b119ccdd18196b53a39a64f153ec79e69ab
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-03-12 11:52:14 +0100

    Modelo User

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  status: { type: Number, default: 0 },
+  role: { type: String, default: "user" },
+  verificationCode: { type: String },
+  attempts: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("User", UserSchema);
