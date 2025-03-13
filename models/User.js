534b57b378509063f725eece31394c981b9f469f
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-03-13 09:32:06 +0100

    Datos personales y empresa

--- models/User.js
+++ models/User.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  status: { type: Number, default: 0 },
-  role: { type: String, default: "user" },
+  status: { type: Number, default: 0 }, // 0: no validado, 1: validado
+  role: { type: String, default: "user" }, // Por ejemplo, "user" o "autonomo"
   verificationCode: { type: String },
-  attempts: { type: Number, default: 0 },
+  attempts: { type: Number, default: 3 },
+  // Datos personales
+  nombre: { type: String, default: "" },
+  apellidos: { type: String, default: "" },
+  nif: { type: String, default: "" },
+  // Datos de la compañía
+  companyName: { type: String, default: "" },
+  cif: { type: String, default: "" },
+  direccion: { type: String, default: "" },
 });
 
 module.exports = mongoose.model("User", UserSchema);
