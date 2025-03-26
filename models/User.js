fdeb7469ff25ea00b38bc831de42d8a1897853e3
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-03-26 12:27:31 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   status: { type: Number, default: 0 }, // 0: no validado, 1: validado
-  role: { type: String, default: "user" }, // Ej: "user" o "autonomo"
+  role: { type: String, default: "user" },
   verificationCode: { type: String },
   attempts: { type: Number, default: 3 },
   // Datos personales
@@ -18,6 +19,12 @@ const UserSchema = new Schema({
   direccion: { type: String, default: "" },
   // Logo
   logo: { type: String, default: "" },
+  // Recuperación de contraseña
+  passwordRecoveryCode: { type: String, default: "" },
 });
+
+// Se aplica el plugin para soft delete. Con { overrideMethods: "all" }
+// se sobrescriben los métodos find, findOne, etc. para no devolver documentos borrados.
+UserSchema.plugin(mongooseDelete, { overrideMethods: "all", deletedAt: true });
 
 module.exports = mongoose.model("User", UserSchema);
