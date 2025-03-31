b4d189028e740456457bdda0c1b53c3c7a60633c
FilliusMahiae <sergio.mahiarello@gmail.com>
2025-03-31 17:39:03 +0200

    Update personalValidator.js

--- validators/personalValidator.js
+++ validators/personalValidator.js
@@ -1,11 +1,17 @@
-// validators/personalValidator.js
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 const validatorResults = require("../utils/handleValidator");
 
 exports.validatePersonalData = [
   check("nombre", "El nombre es obligatorio").notEmpty(),
   check("apellidos", "Los apellidos son obligatorios").notEmpty(),
   check("nif", "El NIF es obligatorio").notEmpty(),
-  // Se puede agregar validación adicional para el formato del NIF
+
+  // Validaciones del objeto address
+  body("address.street", "La calle es obligatoria").notEmpty(),
+  body("address.number", "El número debe ser un entero").isInt().toInt(),
+  body("address.postal", "El código postal debe ser un número").isInt().toInt(),
+  body("address.city", "La ciudad es obligatoria").notEmpty(),
+  body("address.province", "La provincia es obligatoria").notEmpty(),
+
   validatorResults,
 ];
